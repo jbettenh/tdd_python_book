ad1764c3cb056918ded93c863e64df8e98ac58a6
jbettenh <21023218+jbettenh@users.noreply.github.com>
2020-10-10 10:19:25 +0200

    removed extra broser call

--- functional_tests.py
+++ functional_tests.py
@@ -10,8 +10,6 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser = webdriver.Firefox()
-
         # Edith has heard about a cool new online to-do app. She goes to check out its homepage
         self.browser.get('http://localhost:8000')
 
