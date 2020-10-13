520a4ab8abbac89173f7c22d5c000ab8a66b3174
jbettenh <21023218+jbettenh@users.noreply.github.com>
2020-10-13 10:37:44 +0200

    Refactor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -35,7 +35,7 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
-        
+
         # There is still a text box inviting her to add another item
         # She enters "Use peacock feathers to make fly"
         self.fail('Finish the test!')
